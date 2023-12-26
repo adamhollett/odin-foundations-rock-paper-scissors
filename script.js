e5e25460137cac862db2949a15f138f73b1395f4
Adam Hollett <mail@adamhollett.com>
2023-12-26 16:44:53 -0330

    Tidy up

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const CHOICES            = ["rock", "paper", "scissors"];
-const PLAYER_WIN_LABEL   = "player win";
-const COMPUTER_WIN_LABEL = "computer win";
-const TIE_LABEL          = "tie";
-const results            = [];
+const CHOICES = ["rock", "paper", "scissors"];
+const GAMES_PER_ROUND = 5;
 
-function sample(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
+const results = [];
+const score = { player: 0, computer: 0 };
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 function getPlayerChoice() {
   const choice = prompt("Choose rock, paper, or scissors!").toLowerCase();
@@ -23,32 +21,17 @@ function getComputerChoice() {
 }
 
 function handleTie() {
-  const message = "It's a tie, play again!"
-  console.info(message);
-
-  return results.push(TIE_LABEL);
+  console.info("Round is a tie!");
 }
 
 function handlePlayerWin() {
-  const message = "You win! Great job!"
-  console.info(message);
-
-  return results.push(PLAYER_WIN_LABEL);
+  console.info("Player wins the round!");
+  return score.player++;
 }
 
 function handlePlayerLoss() {
-  const message = "You lose! Better luck next time!"
-  console.info(message);
-
-  return results.push(COMPUTER_WIN_LABEL);
-}
-
-function numPlayerWins() {
-  return results.filter((item) => item === PLAYER_WIN_LABEL).length;
-}
-
-function numComputerWins() {
-  return results.filter((item) => item === COMPUTER_WIN_LABEL).length;
+  console.info("Computer wins the round!");
+  return score.computer++;
 }
 
 function playRound(playerChoice = getPlayerChoice(), computerChoice = getComputerChoice()) {
@@ -81,23 +64,23 @@ function playRound(playerChoice = getPlayerChoice(), computerChoice = getCompute
 }
 
 function game() {
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < GAMES_PER_ROUND; i++) {
     console.info(`Game ${i}:`);
     playRound();
-    console.info(`Results so far: ${results.join(", ")}`)
-    console.info("\n");
+    console.info(`Results so far: Player: ${score.player} / Computer: ${score.computer}`);
+    console.log("\n");
   }
 
-  if (numPlayerWins() > numComputerWins()) {
-    console.info("You win! Way to go!");
+  if (score.player > score.computer) {
+    return console.info("You win! Way to go!");
   }
 
-  if (numComputerWins() > numPlayerWins()) {
-    console.info("Computer wins! Better luck next time!");
+  if (score.computer > score.player) {
+    return console.info("Computer wins! Better luck next time!");
   }
 
-  if (numPlayerWins() === numComputerWins()) {
-    console.info("Somehow it's a tie! What are the odds!");
+  if (score.player === score.computer) {
+    return console.info("It's a tie! What are the odds!");
   }
 }
 
